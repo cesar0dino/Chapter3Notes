da9c4b666579591a95b6354c99440b384d757598
de6y <de6y@fresnou.org>
2023-03-14 10:29:23 -0700

    functions notes

--- FunctionsNotes.js
+++ FunctionsNotes.js
@@ -2,38 +2,38 @@
 
 // 1. define the function
 
-// function sayHello(){
-//
-// console.log('Hello');
-//
-// }
+function sayHello(){
+
+console.log('Hello');
+
+}
 
 // 2. call (or use) the function
-// sayHello();
+sayHello();
 
-// function printMax(){
-//     let x = 6;
-//     let y = 29;
-//     if (x>y){
-//         console.log(`the larger number is ${x}`);
-//     } else {
-//         console.log(`the larger number is ${y}`);
-//     }
-// }
-//
-// printMax();
+function printMax(){
+    let x = 6;
+    let y = 29;
+    if (x>y){
+        console.log(`the larger number is ${x}`);
+    } else {
+        console.log(`the larger number is ${y}`);
+    }
+}
 
-// function printMax(x, y){
-//     if (x>y){
-//         console.log(`the larger number is ${x}`);
-//     } else {
-//         console.log(`the larger number is ${y}`);
-//     }
-// }
-//
-// printMax(16, 9);
-// printMax(2, 75);
-// printMax(2, 2);
+printMax();
+
+function printMax(x, y){
+    if (x>y){
+        console.log(`the larger number is ${x}`);
+    } else {
+        console.log(`the larger number is ${y}`);
+    }
+}
+
+printMax(16, 9);
+printMax(2, 75);
+printMax(2, 2);
 
 function sum(num1, num2){
     console.log(`${num1}+${num2}=${num1+num2}`);
---
+++ FunctionsPractice.js
@@ -0,0 +1,73 @@
+//1
+// function sum(num1, num2){
+//     console.log(`${num1}*${num2}=${num1*num2}`);
+// }
+//
+// sum(23, 31);
+//
+// //2
+// function sum(num1, num2){
+//     console.log(`${num1}*${num2}=${num1*num2}`);
+// }
+//
+// sum(18, 3.14);
+//
+// //3
+// function printMax(){
+//     let x = 14;
+//     let y = 52;
+//     if (x<y){
+//         console.log(`the smallest number is ${x}`);
+//     } else {
+//         console.log(`the smallest number is ${y}`);
+//     }
+// }
+//
+// printMax();
+//
+// //4
+// function printMax(x, y){
+//     if (x>y){
+//         console.log(`Both the numbers are equal ${x}`);
+//     } else {
+//         console.log(`Both the numbers are equal ${y}`);
+//     }
+// }
+//
+// printMax(2, 2);
+//
+// //5
+// function sum(num1, num2){
+//     console.log(`${num1}*${num2}=${num1*num2}`);
+// }
+//
+// sum1(23, 31);
+//
+// function sum1(num3, num4){
+//     console.log(`${num3}*${num4}=${num3*num4}`);
+// }
+//
+// sum1(54, 28);
+//
+// function printMax(){
+//     let sum = 713;
+//     let sum1 = 1512;
+//     if (sum>sum1){
+//         console.log(`the largest area is ${sum}`);
+//     } else {
+//         console.log(`the largest area is ${sum1}`);
+//     }
+// }
+//
+// printMax();
+//
+//6
+function sum(num1, num2, num3){
+    console.log(`${num1}+${num2}+${num3}=${num1+num2+num3}`);
+}
+
+sum(375, 297, 412);
+
+
+
+
