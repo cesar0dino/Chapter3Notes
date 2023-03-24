0a9d653d38e59f038f37dafae2a39cfcd153ee03
de6y <de6y@fresnou.org>
2023-03-24 10:26:38 -0700

    ArrayNotes

---
+++ Arrays1.js
@@ -0,0 +1,15 @@
+//1
+function firstElement1(arr){
+    console.log(arr[0]);
+}
+let myNums1 = [1, 2, 3, 4];
+firstElement1 (myNums1);
+
+//2
+function firstElement(arr){
+    console.log(arr[3]);
+}
+let myNums = [1, 2, 3, 4];
+firstElement (myNums);
+
+//3
