be190066b3e68d5a7bdd35bcb446852e9240aa04
de6y <de6y@fresnou.org>
2023-03-29 10:27:39 -0700

    Arrays2Notes

--- Arrays2.js
+++ Arrays2.js
@@ -5,3 +5,17 @@ console.log('if n =5: ' + nums);
 console.log('if n =2: ' + newNums);
 
 //2
+function countUp(n){
+    if (n <= 0){
+        console.log('Invalid')
+    }
+    else( n >=0);
+    let numberList = [];
+    for (let i=1; i<=n; i++){
+        numberList.push(i);
+    }
+    console.log(numberList)
+}
+countUp(-1);
+
+//3
